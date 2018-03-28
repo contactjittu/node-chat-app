b52c126a9a2593225450826a638656b2c3b52839
Jitendra Kumar <jitendra.kumar@neosofttech.om>
2018-03-28 17:56:06 +0530

    create generateMessage utilities

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const { generateMessage } = require('./utils/message')
 const publicPath = path.join(__dirname, '../public');
 let app = express();
 let port = process.env.PORT || 3000;
@@ -15,9 +16,25 @@ let io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', generateMessage('Adam','Welcome to Jungle'));
+
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New user Joined'));
+
+    socket.on('createMessage', (message)=> {
+        console.log('Create message: ', message);
+        io.emit('newMessage',generateMessage(message.from, message.text));
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
+    })
+
     socket.on('disconnect', ()=> {
         console.log('User disconnected');
-    })
+    });
+
 })
 
 server.listen(port, ()=> {
