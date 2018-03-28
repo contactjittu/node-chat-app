756159f1acf2eed74aff83e28132ab3edf8e5b3f
Jitendra Kumar <jitendra.kumar@neosofttech.om>
2018-03-28 12:40:56 +0530

    socket added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
+'use strict';
+
+const http = require('http');
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 let app = express();
 let port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', ()=> {
+        console.log('User disconnected');
+    })
+})
+
+server.listen(port, ()=> {
     console.log(`Server is listining on port : ${port}`);
 })
