c52ed8efbe12cfffb0b3cd54df12f2f434243477
Jitendra Kumar <jitendra.kumar@neosofttech.om>
2018-03-28 20:51:07 +0530

    add css

--- public/js/index.js
+++ public/js/index.js
@@ -23,11 +23,12 @@ socket.on('newLocationMessage', function(message){
 
 jQuery('#message-from').on('submit', function(e){
     e.preventDefault();
+    let messageTextBox = jQuery('[name=message]')
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function(){
-
+        messageTextBox.val('')
     })
 })
 
@@ -36,12 +37,17 @@ locationButton.on('click', function(){
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location....');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function(){
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     })
 })
